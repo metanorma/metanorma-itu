445ebcde9872df2b81b76aca0f9c8ac072d6efbd
Nick Nicholas <opoudjis@gmail.com>
2019-09-26 19:01:27 +1000

    rspec

--- spec/asciidoctor/base_spec.rb
+++ spec/asciidoctor/base_spec.rb
@@ -750,8 +750,7 @@ INPUT
             #{BLANK_HDR}
             <preface/><sections>
          <formula id="_" inequality="true" unnumbered="true">
-         <stem type="AsciiMath">r = 1 %
-       r = 1 %</stem>
+         <stem type="MathML"><math xmlns="http://www.w3.org/1998/Math/MathML"><mi>r</mi><mo>=</mo><mn>1</mn><mi>%</mi><mi>r</mi><mo>=</mo><mn>1</mn><mi>%</mi></math></stem>
        </formula>
        </sections>
        </itu-standard>

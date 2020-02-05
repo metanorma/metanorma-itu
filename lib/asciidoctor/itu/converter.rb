027a3805f3c665d02d9f14a13b6a81e7dc5fec57
Nick Nicholas <opoudjis@gmail.com>
2020-02-06 00:52:03 +1100

    refactor section names: https://github.com/metanorma/metanorma-iso/issues/313

--- lib/asciidoctor/itu/converter.rb
+++ lib/asciidoctor/itu/converter.rb
@@ -101,26 +101,25 @@ module Asciidoctor
         return
       end
 
+      def sectiontype_streamline(ret)
+        case ret
+        when "definitions" then "terms and definitions"
+        when "abbreviations and acronyms" then "symbols and abbreviated terms"
+        else
+          super
+        end
+      end
+
       def section(node)
         a = section_attributes(node)
         noko do |xml|
           case sectiontype(node)
           when "references" then norm_ref_parse(a, xml, node)
-          when "terms and definitions",
-            "terms, definitions, symbols and abbreviated terms",
-            "terms, definitions, symbols and abbreviations",
-            "terms, definitions and symbols",
-            "terms, definitions and abbreviations",
-            "terms, definitions and abbreviated terms",
-            "definitions"
+          when "terms and definitions"
             @term_def = true
             term_def_parse(a, xml, node, true)
             @term_def = false
-          when "symbols and abbreviated terms",
-            "symbols",
-            "abbreviated terms",
-            "abbreviations",
-            "abbreviations and acronyms"
+          when "symbols and abbreviated terms"
             symbols_parse(a, xml, node)
           when "bibliography" then bibliography_parse(a, xml, node)
           else

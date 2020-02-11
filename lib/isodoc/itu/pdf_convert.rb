f2e91373138cf3991eb96b90538f1b4a1e671325
Nick Nicholas <opoudjis@gmail.com>
2020-02-11 23:26:21 +1100

    add xsl fo support: https://github.com/metanorma/isodoc/issues/167

--- lib/isodoc/itu/pdf_convert.rb
+++ lib/isodoc/itu/pdf_convert.rb
@@ -6,61 +6,22 @@ module IsoDoc
   module ITU
     # A {Converter} implementation that generates PDF HTML output, and a
     # document schema encapsulation of the document for validation
-    class PdfConvert < IsoDoc::PdfConvert
+    class PdfConvert < IsoDoc::XslfoPdfConvert
       def initialize(options)
         @libdir = File.dirname(__FILE__)
         @hierarchical_assets = options[:hierarchical_assets]
         super
       end
 
-      def html_toc(docxml)
-        docxml
+      def convert(filename, file = nil, debug = false)
+        file = File.read(filename, encoding: "utf-8") if file.nil?
+        docxml, outname_html, dir = convert_init(file, filename, debug)
+        resolution = docxml.at(ns("//bibdata/ext[doctype = 'resolution']"))
+        FileUtils.rm_rf dir
+        ::Metanorma::Output::XslfoPdf.new.convert(
+          filename, outname_html + ".pdf",
+          File.join(@libdir, resolution ? "itu.resolution.xsl" : "itu.recommendation.xsl"))
       end
-
-      def default_fonts(options)
-        {
-          bodyfont: (options[:script] == "Hans" ? '"SimSun",serif' : '"Open Sans",sans-serif'),
-          headerfont: (options[:script] == "Hans" ? '"SimHei",sans-serif' : '"Open Sans",sans-serif'),
-          monospacefont: '"Space Mono",monospace'
-        }
-      end
-
-      def default_file_locations(_options)
-        {
-          htmlstylesheet: html_doc_path("htmlstyle.scss"),
-          htmlcoverpage: html_doc_path("html_itu_titlepage.html"),
-          htmlintropage: html_doc_path("html_itu_intro.html"),
-          scripts_pdf: html_doc_path("scripts.pdf.html"),
-        }
-      end
-
-      def googlefonts()
-        <<~HEAD.freeze
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,800|Space+Mono:400,700" rel="stylesheet">
-        HEAD
-      end
-
-      def make_body(xml, docxml)
-        body_attr = { lang: "EN-US", link: "blue", vlink: "#954F72", "xml:lang": "EN-US", class: "container" }
-        xml.body **body_attr do |body|
-          make_body1(body, docxml)
-          make_body2(body, docxml)
-          make_body3(body, docxml)
-        end
-      end
-
-      def make_body3(body, docxml)
-        body.div **{ class: "main-section" } do |div3|
-          boilerplate docxml, div3
-          abstract docxml, div3
-          preface docxml, div3
-          middle docxml, div3
-          footnotes div3
-          comments div3
-        end
-      end
-
-      include BaseConvert
     end
   end
 end

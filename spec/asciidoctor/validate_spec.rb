6cda153aa3016de6cc258647071ed0d7761a5596
Тараскин Михаил <MITaraskin@domclick.ru>
2020-10-09 16:39:50 +0300

    metanorma/metanorma-standoc#225 add error file generation test

--- spec/asciidoctor/validate_spec.rb
+++ spec/asciidoctor/validate_spec.rb
@@ -2,6 +2,26 @@ require "spec_helper"
 require "fileutils"
 
 RSpec.describe Asciidoctor::ITU do
+  context "when xref_error.adoc compilation" do
+    around do |example|
+      FileUtils.rm_f "spec/assets/xref_error.err"
+      example.run
+      Dir["spec/assets/xref_error*"].each do |file|
+        next if file.match?(/adoc$/)
+
+        FileUtils.rm_f(file)
+      end
+    end
+
+    it "generates error file" do
+      expect do
+        Metanorma::Compile
+          .new
+          .compile("spec/assets/xref_error.adoc", type: "itu")
+      end.to(change { File.exist?("spec/assets/xref_error.err") }
+              .from(false).to(true))
+    end
+  end
 
   it "Warns of illegal doctype" do
       FileUtils.rm_f "test.err"
@@ -85,7 +105,7 @@ it "Warns if term definition does not start with capital" do
   Asciidoctor.convert(<<~"INPUT", backend: :itu, header_footer: true)
   #{VALIDATING_BLANK_HDR}
   == Terms and Definitions
-  
+
   === Term
 
   the definition of a term is a part of the specialized vocabulary of a particular field
@@ -98,7 +118,7 @@ it "Warns if term definition does not end with period" do
   Asciidoctor.convert(<<~"INPUT", backend: :itu, header_footer: true)
   #{VALIDATING_BLANK_HDR}
   == Terms and Definitions
-  
+
   === Term
 
   Part of the specialized vocabulary of a particular field

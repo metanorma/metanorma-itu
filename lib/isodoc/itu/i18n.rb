6513f1096660135c3e7d3158358ed1fbf21c11cc
Nick Nicholas <opoudjis@gmail.com>
2021-04-05 15:09:36 +1000

    ITU Chinese i18n: https://github.com/metanorma/metanorma-itu/issues/272

--- lib/isodoc/itu/i18n.rb
+++ lib/isodoc/itu/i18n.rb
@@ -1,9 +1,12 @@
 module IsoDoc
   module ITU
     class I18n < IsoDoc::I18n
-       def load_yaml1(lang, script)
+      def load_yaml1(lang, script)
         y = if lang == "en"
               YAML.load_file(File.join(File.dirname(__FILE__), "i18n-en.yaml"))
+            elsif lang == "zh" && script == "Hans"
+              YAML.load_file(File.join(File.dirname(__FILE__),
+                                       "i18n-zh-Hans.yaml"))
             else
               YAML.load_file(File.join(File.dirname(__FILE__), "i18n-en.yaml"))
             end

f7a7015aaa0d05516e8fff5ace55fe6c967ff8f6
Nick Nicholas <opoudjis@gmail.com>
2021-04-08 15:22:39 +1000

    inherit scripts.html: https://github.com/metanorma/isodoc/issues/284

--- lib/isodoc/itu/html_convert.rb
+++ lib/isodoc/itu/html_convert.rb
@@ -33,7 +33,6 @@ module IsoDoc
           htmlstylesheet: html_doc_path("htmlstyle.scss"),
           htmlcoverpage: html_doc_path("html_itu_titlepage.html"),
           htmlintropage: html_doc_path("html_itu_intro.html"),
-          scripts: html_doc_path("scripts.html"),
         }
       end
 

e7e9ed6b3833a769f826de364081db6517f18276
Nick Nicholas <opoudjis@gmail.com>
2022-09-25 21:23:56 +1000

    debug unsafe implementation of figure xrefs, align with asset names refactoring in isodoc: https://github.com/metanorma/mn-samples-itu/issues/148

--- lib/isodoc/itu/xref.rb
+++ lib/isodoc/itu/xref.rb
@@ -24,7 +24,7 @@ module IsoDoc
         super
         if @parse_settings.empty? || @parse_settings[:clauses]
           if annexid = docxml
-            &.at(ns("//bibdata/ext/structuredidentifier/annexid"))&.text
+              .at(ns("//bibdata/ext/structuredidentifier/annexid"))&.text
             docxml.xpath(ns("//annex")).each { |c| annex_names(c, annexid) }
           else
             informative_annex_names(docxml)
@@ -57,14 +57,6 @@ module IsoDoc
           doc.xpath(ns("//boilerplate//clause")).each { |c| preface_names(c) }
           doc.xpath("//xmlns:preface/child::*").each { |c| preface_names(c) }
         end
-        if @parse_settings.empty?
-          if @hierarchical_assets
-            hierarchical_asset_names(doc.xpath("//xmlns:preface/child::*"),
-                                     "Preface")
-          else
-            sequential_asset_names(doc.xpath("//xmlns:preface/child::*"))
-          end
-        end
         if @parse_settings.empty? || @parse_settings[:clauses]
           n = Counter.new
           n = section_names(doc.at(ns("//clause[@type = 'scope']")), n, 1)
@@ -85,6 +77,18 @@ module IsoDoc
           "//sections/definitions | //clause[parent::sections]"
       end
 
+      def asset_anchor_names(doc)
+        super
+        if @parse_settings.empty?
+          if @hierarchical_assets
+            hierarchical_asset_names(doc.xpath("//xmlns:preface/child::*"),
+                                     "Preface")
+          else
+            sequential_asset_names(doc.xpath("//xmlns:preface/child::*"))
+          end
+        end
+      end
+
       def middle_section_asset_names(doc)
         return super unless @hierarchical_assets
 
@@ -93,47 +97,28 @@ module IsoDoc
         end
       end
 
-      def sequential_figure_names(clause)
-        c = Counter.new
-        j = 0
-        clause.xpath(ns(".//figure | .//sourcecode[not(ancestor::example)]")).each do |t|
-          if t.parent.name == "figure" then j += 1
-          else
-            j = 0
-            c.increment(t)
-          end
-          label = c.print + (j.zero? ? "" : "#{hierfigsep}#{(96 + j).chr}")
-          next if t["id"].nil? || t["id"].empty?
-
-          @anchors[t["id"]] =
-            anchor_struct(label, nil, @labels["figure"], "figure",
-                          t["unnumbered"])
-        end
+      def sequential_figure_body(subfignum, counter, block)
+        label = counter.print
+        label &&= label +
+          (subfignum.zero? ? "" : "#{hierfigsep}#{(subfignum + 96).chr}")
+        @anchors[block["id"]] = anchor_struct(
+          label, nil, @labels["figure"], "figure", block["unnumbered"]
+        )
       end
 
-      def hierarchical_figure_names(clause, num)
-        c = Counter.new
-        j = 0
-        clause.xpath(ns(".//figure | .//sourcecode[not(ancestor::example)]")).each do |t|
-          if t.parent.name == "figure" then j += 1
-          else
-            j = 0
-            c.increment(t)
-          end
-          label = "#{num}#{hiersep}#{c.print}" + (j.zero? ? "" : "#{hierfigsep}#{(96 + j).chr}")
-          next if t["id"].nil? || t["id"].empty?
+      def hierarchical_figure_body(num, subfignum, counter, block)
+        label = "#{num}#{hiersep}#{counter.print}" +
+          (subfignum.zero? ? "" : "#{hierfigsep}#{(subfignum + 96).chr}")
 
-          @anchors[t["id"]] =
-            anchor_struct(label, nil, @labels["figure"], "figure",
-                          t["unnumbered"])
-        end
+        @anchors[block["id"]] = anchor_struct(label, nil, @labels["figure"],
+                                              "figure", block["unnumbered"])
       end
 
       def sequential_formula_names(clause)
-        clause&.first&.xpath(ns(middle_sections))&.each do |c|
+        clause.first&.xpath(ns(middle_sections))&.each do |c|
           if c["id"] && @anchors[c["id"]]
-            hierarchical_formula_names(c,
-                                       @anchors[c["id"]][:label] || @anchors[c["id"]][:xref] || "???")
+            hierarchical_formula_names(c, @anchors[c["id"]][:label] ||
+                                       @anchors[c["id"]][:xref] || "???")
           else
             hierarchical_formula_names(c, "???")
           end
@@ -142,9 +127,7 @@ module IsoDoc
 
       def hierarchical_formula_names(clause, num)
         c = Counter.new
-        clause.xpath(ns(".//formula")).reject do |n|
-          blank?(n["id"])
-        end.each do |t|
+        clause.xpath(ns(".//formula")).noblank.each do |t|
           @anchors[t["id"]] = anchor_struct(
             "#{num}-#{c.increment(t).print}", nil,
             t["inequality"] ? @labels["inequality"] : @labels["formula"],
@@ -163,7 +146,7 @@ module IsoDoc
         docxml.xpath(ns("//term[termnote]")).each do |t|
           c = Counter.new
           notes = t.xpath(ns("./termnote"))
-          notes.reject { |n| blank?(n["id"]) }.each do |n|
+          notes.noblank.each do |n|
             idx = notes.size == 1 ? "" : " #{c.increment(n).print}"
             @anchors[n["id"]] =
               { label: termnote_label(idx).strip, type: "termnote", value: idx,

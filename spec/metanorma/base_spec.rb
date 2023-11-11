eb61709b2f379ac334f0a53cdc1b42a1cd5225d0
Nick Nicholas <opoudjis@gmail.com>
2023-11-11 11:08:44 +1100

    rspec

--- spec/metanorma/base_spec.rb
+++ spec/metanorma/base_spec.rb
@@ -2082,7 +2082,8 @@ RSpec.describe Metanorma::ITU do
          <docidentifier type="ISO" primary="true">ISO/IEC 27001</docidentifier>
          <docidentifier type="iso-reference">ISO/IEC 27001(E)</docidentifier>
          <docidentifier type="URN">urn:iso:std:iso-iec:27001:stage-60.60:ed-3</docidentifier>
-          <docidentifier type="IEC">IEC 60027</docidentifier>
+          <docidentifier type="IEC" primary="true">IEC 60027</docidentifier>
+          <docidentifier type="URN">urn:iec:std:iec:60027::::</docidentifier>
           </div>
         OUTPUT
     end

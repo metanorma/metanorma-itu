57a2ddd5c1af73363c4074ec19e7a27d1e101678
Nick Nicholas <opoudjis@gmail.com>
2023-11-30 01:24:42 +1100

    refactor i18n yaml to prevent overwriting parent non-English YAML with local English file: https://github.com/metanorma/isodoc/issues/549

--- lib/isodoc/itu/i18n.rb
+++ lib/isodoc/itu/i18n.rb
@@ -1,22 +1,14 @@
 module IsoDoc
   module ITU
     class I18n < IsoDoc::I18n
-      def load_yaml2x(str)
-        YAML.load_file(File.join(File.dirname(__FILE__),
-                                 "i18n-#{str}.yaml"))
+      def load_file(fname)
+        f = File.join(File.dirname(__FILE__), fname)
+        File.exist?(f) ? YAML.load_file(f) : {}
       end
 
       def load_yaml1(lang, script)
-        y = case lang
-            when "en", "fr", "ru", "de", "es", "ar"
-              load_yaml2x(lang)
-            when "zh"
-              if script == "Hans" then load_yaml2x("zh-Hans")
-              else load_yaml2x("en")
-              end
-            else load_yaml2x("en")
-            end
-        super.deep_merge(y)
+        y = load_file("i18n-#{yaml_lang(lang, script)}.yaml")
+        y.empty? ? load_file("i18n-en.yaml").merge(super) : super.deep_merge(y)
       end
     end
   end
